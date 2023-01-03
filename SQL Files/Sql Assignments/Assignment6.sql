59ef42076dd7d2475ee1290f821fdf42178af0b8
ramakrishnaprasadS <121850384+ramakrishnaprasadS@users.noreply.github.com>
2023-01-03 15:31:26 +0530

    apdated assignment6

--- SQL Files/Sql Assignments/Assignment6.sql
+++ SQL Files/Sql Assignments/Assignment6.sql
@@ -1,25 +1,61 @@
 -- Active: 1671708805339@@127.0.0.1@3308@hr_db
 
+use world;
 
+show tables;
 
+select * from countries;
+select * from departments;
+select * from jobs;
+select * from employees;
+select * from job_history;
+select * from locations;
+select * from regions;
 
---1. **Select employees first name, last name, job_id and salary whose first name starts with alphabet S.**
---2. **Write a query to select employee with the highest salary.**
---3. **Select employee with the second highest salary**.
---4. **Fetch employees with 2nd or 3rd highest salary**.
---5. **Write a query to select employees and their corresponding managers and their salaries**.
---6. **Write a query to show count of employees under each manager in descending order**.
---7. **Find the count of employees in each department**.
---8. **Get the count of employees hired year wise**.
---9. **Find the salary range of employees**.
---10. **Write a query to divide people into three groups based on their salaries**.
---11. **Select the employees whose first_name contains “an”.**
---12. **Select employee first name and the corresponding phone number in the format (_ _ _)-(_ _ _)-(_ _ _ _)**.
---13. **Find the employees who joined in August, 1994.**
---14. **Write an SQL query to display employees who earn more than the average salary in that company**
---15. **Find the maximum salary from each department.**
---16. **Write a SQL query to display the 5 least earning employees**.
---17. **Find the employees hired in the 80s**.
---18. **Display the employees first name and the name in reverse order**.
---19. **Find the employees who joined the company after 15th of the month.**
---20. **Display the managers and the reporting employees who work in different departments**.
+--1. **Write a query to list the number of jobs available in the employees table.**
+select count(distinct job_id) as jobs_count from employees;
+
+--2. **Write a query to get the total salaries payable to employees.**
+select sum(salary) as total_payable from employees;
+
+--3. **Write a query to get the minimum salary from the employees table.**
+select min(salary) as min_salary from employees;
+
+--4. **Write a query to get the maximum salary of an employee working as a Programmer.**
+select  max(e.salary) as max_sal from employees e INNER JOIN jobs j using(job_id) where j.job_title="Programmer";
+
+--5. **Write a query to get the average salary and number of employees working in department 90.**
+select department_id,avg(salary) as avg_sal,count(employee_id) as cnt from employees where department_id=90;
+
+--6. **Write a query to get the highest, lowest, sum, and average salary of all employees**
+select 
+max(salary) as max_sal,
+min(salary) as min_sal,
+sum(salary) as total_sal,
+avg(salary) as avg_sal from employees;
+
+--7. **Write a query to get the number of employees with the same job.**
+select job_id,count(*) as cnt from employees group by job_id with rollup having count(employee_id)>1; 
+--8. **Write a query to get the difference between the highest and lowest salaries.**
+select max(salary)-min(salary) as salary_range from employees;
+
+--9. **Write a query to find the manager ID and the salary of the lowest-paid employee for that manager.**
+select manager_id,min(salary) as min_sal from employees group by manager_id ;
+
+--10. **Write a query to get the department ID and the total salary payable in each department.**
+select department_id,sum(salary) as total_sal_payable from employees group by department_id;
+
+--11. **Write a query to get the average salary for each job ID excluding programmer.**
+select job_id,avg(salary) as avg_sal from employees where job_id not in (select job_id from jobs where job_title="Programmer") group by job_id;
+--12. **Write a query to get the total salary, maximum, minimum, average salary of employees (job ID wise), for department ID 90 only.**
+select job_id,
+sum(salary) as total_sal,
+max(salary) as max_sal,
+min(salary) as min_sal,
+avg(salary) as avg_sal from employees 
+where department_id=90
+group by job_id;
+--13. **Write a query to get the job ID and maximum salary of the employees where maximum salary is greater than or equal to $4000.**
+select job_id,max(salary) as max_sal from employees group by job_id having max(salary)>4000;
+--14. **Write a query to get the average salary for all departments employing more than 10 employees.**
+select department_id,count(employee_id) as cnt,avg(salary) as avg_sal from employees group by department_id having count(employee_id)>10;
