77f11ea5b9b1b36e3bd7c3c5eb2f6436cf98d556
ramakrishnaprasadS <121850384+ramakrishnaprasadS@users.noreply.github.com>
2023-01-24 12:39:44 +0530

    add

--- python/sample.py
+++ python/sample.py
@@ -1,4 +1,26 @@
-fileObj4=open("C:\Users\Public\Documents\week3_Assessment.txt",'r')
-d=fileObj4.read()
-print(d)
-fileObj4.close()
+filename1="C:\\Users\\miles\\Documents\\GitHub\\Training_futurense\\Python\\file1.txt"
+filename2="C:\\Users\\miles\\Documents\\GitHub\\Training_futurense\\Python\\file2.txt"
+try:
+    f=open(filename1,'r')
+    d=f.read()
+    print(d)
+    #a=4/0
+    f.close()
+except FileNotFoundError as fnf:
+    print("the file \n"+filename1+"  \nis not available")
+    try:
+        f=open(filename2,'r')
+        d=f.read()
+        print("file opened from alternate location "+filename2)
+        print(d)
+        f.close()
+    except FileNotFoundError as fnf2:
+        print("file not found in alternate location also")
+    except Exception:
+        print("unexpected error 2")
+    
+except Exception as e:
+    print("Unexpected Error1  is "+str(e))
+finally:
+    f.close()
+    
